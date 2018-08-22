95eec7981a16de0a47111efdccb1e2173809b727
Haggai Philip Zagury <hagzag@tikalk.com>
2018-08-22 21:50:18 +0300

    Add config support for msa-api + different configs for labs

--- msa-api/api.js
+++ msa-api/api.js
@@ -1,11 +1,27 @@
 let express = require('express');
 let redis = require('redis');
 let app = express();
+var config = require('config');
+
+// configuration related
+process.env.NODE_ENV || 'development'
+var app_listen_port = process.env.LISTEN_PORT || 8080;
+
+if (config.has('redis_url')) {
+  var cache_host = config.redis_url;
+  console.log('NODE_CONFIG_DIR: ' + config.util.getEnv('NODE_CONFIG_DIR'));
+  console.log('redis_url loaded for ' + config.util.getEnv('NODE_ENV') + ' environment');
+}
+
+if (process.env.REDIS_URL != null) {
+  var cache_host = process.env.REDIS_URL;
+}
 
 app.set('redis', redis.createClient({
-  url: process.env.REDIS_URL
+  url: cache_host
 }));
 
+// routes
 app.get('/', function (req, res) {
   let client = app.get('redis');
 
@@ -14,6 +30,19 @@ app.get('/', function (req, res) {
       res.status(500).send(err);
     } else {
       res.send("Current ping count: " + result + "\n");
+    }
+  });
+});
+
+app.get('/pinger', function (req, res) {
+  let client = app.get('redis');
+
+  client.incrby('pings', "1", function(err, result) {
+    if(err) {
+      res.status(500).send(err);
+    } else {
+      res.send('Ping + 1')
+      res.status(204).end();
     }
   });
 });
@@ -50,6 +79,7 @@ app.get('/probe/readiness', function (req, res) {
   });
 });
 
-app.listen(8080, function () {
-	console.log('Server running on port 8080!');
+app.listen(app_listen_port, function () {
+  console.log('running on host ' + config.util.getEnv('HOSTNAME'));
+  console.log('Server running on port ' + app_listen_port + '!');
 });
